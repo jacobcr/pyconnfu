e4d537af7f4640320a1deb2d14d47beaa9faa75c
jacob <jacob83@gmail.com>
2011-11-19 10:55:24 +0100

    First version of connfu module

---
+++ connfu/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+u'''
+Created on 4 Oct 2011
+
+@author: jacob <jcr@tid.es>
+'''
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+u'''
+created on 7 nov 2011
+
+@author: jacob <jcr@tid.es>
+'''
+
+from distutils.core import setup
+
+setup(name='pyconnfu',
+      version='0.1',
+      description='first version of python client',
+      author='jacob cr',
+      author_email='jcr@tid.es',
+      packages=['connfu'],
+     )
+
